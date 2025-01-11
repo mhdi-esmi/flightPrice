f381ac2265fa1726b22c929843d20506813ad1e9
Mhdi <mhdi.esmi@gmail.com>
2025-01-11 19:14:57 +0330

    add data model and repository

--- src/main/java/com/mhdi/flightPrice/model/Flight.java
+++ src/main/java/com/mhdi/flightPrice/model/Flight.java
@@ -1,5 +1,38 @@
 package com.mhdi.flightPrice.model;
 
-public class Flight {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Flight {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String origin;
+    private String destination;
+    private String date;
+    private String flightNumber;
+    private double price;
+
+    @Override
+    public String toString() {
+        return "Flight{" +
+                "id=" + id +
+                ", origin='" + origin + '\'' +
+                ", destination='" + destination + '\'' +
+                ", date='" + date + '\'' +
+                ", flightNumber='" + flightNumber + '\'' +
+                ", price=" + price +
+                '}';
+    }
 }
--- src/main/java/com/mhdi/flightPrice/repository/FlightRepository.java
+++ src/main/java/com/mhdi/flightPrice/repository/FlightRepository.java
@@ -1,5 +1,14 @@
 package com.mhdi.flightPrice.repository;
 
-public interface FlightRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.mhdi.flightPrice.model.Flight;
+
+
+@Repository
+public interface FlightRepository extends JpaRepository<Flight, Long> {
+    List<Flight> findByOriginAndDestinationAndDate(String origin, String destination, String date);
 }
